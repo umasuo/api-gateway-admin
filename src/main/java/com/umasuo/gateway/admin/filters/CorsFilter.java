94c3b7763774ab15d645462489dceb760d1cb4e9
davis999 <davis.dai@go6d.com>
2017-08-25 14:50:50 +0800

    edit origin

--- src/main/java/com/umasuo/gateway/admin/filters/CorsFilter.java
+++ src/main/java/com/umasuo/gateway/admin/filters/CorsFilter.java
@@ -32,7 +32,7 @@ public class CorsFilter implements Filter {
       throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) resp;
     HttpServletRequest request = (HttpServletRequest) req;
-    response.setHeader("Access-Control-Allow-Origin", "*");
+//    response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
     response.setHeader("Access-Control-Max-Age", "3600");
     response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
