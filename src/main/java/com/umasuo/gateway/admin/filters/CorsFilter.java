5b05aec9afc0b6687c1f6c76473f053e323c24a1
davis999 <davis.dai@go6d.com>
2017-08-25 11:46:50 +0800

    add cors filter

---
+++ src/main/java/com/umasuo/gateway/admin/filters/CorsFilter.java
@@ -0,0 +1,43 @@
+package com.umasuo.gateway.admin.filters;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Davis on 17/8/25.
+ */
+@Component
+public class CorsFilter extends OncePerRequestFilter {
+  static final String ORIGIN = "Origin";
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    System.out.println(request.getHeader(ORIGIN));
+    System.out.println(request.getMethod());
+    if (request.getHeader(ORIGIN).equals("null")) {
+      String origin = request.getHeader(ORIGIN);
+      response.setHeader("Access-Control-Allow-Origin", "*");//* or origin as u prefer
+      response.setHeader("Access-Control-Allow-Credentials", "true");
+      response.setHeader("Access-Control-Allow-Headers",
+          request.getHeader("Access-Control-Request-Headers"));
+    }
+    if (request.getMethod().equals("OPTIONS")) {
+      try {
+        response.getWriter().print("OK");
+        response.getWriter().flush();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }else{
+      filterChain.doFilter(request, response);
+    }
+  }
+}
