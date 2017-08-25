73195d283a5317e98af7fbdb16e609711bfd4fac
davis999 <davis.dai@go6d.com>
2017-08-25 12:47:11 +0800

    fix cors bug

--- src/main/java/com/umasuo/gateway/admin/filters/CorsFilter.java
+++ src/main/java/com/umasuo/gateway/admin/filters/CorsFilter.java
@@ -1,12 +1,18 @@
 package com.umasuo.gateway.admin.filters;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,30 +20,32 @@ import javax.servlet.http.HttpServletResponse;
  * Created by Davis on 17/8/25.
  */
 @Component
-public class CorsFilter extends OncePerRequestFilter {
-  static final String ORIGIN = "Origin";
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
 
   @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-      throws ServletException, IOException {
-    System.out.println(request.getHeader(ORIGIN));
-    System.out.println(request.getMethod());
-    if (request.getHeader(ORIGIN).equals("null")) {
-      String origin = request.getHeader(ORIGIN);
-      response.setHeader("Access-Control-Allow-Origin", "*");//* or origin as u prefer
-      response.setHeader("Access-Control-Allow-Credentials", "true");
-      response.setHeader("Access-Control-Allow-Headers",
-          request.getHeader("Access-Control-Request-Headers"));
+  public void init(FilterConfig fc) throws ServletException {
+  }
+
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletResponse response = (HttpServletResponse) resp;
+    HttpServletRequest request = (HttpServletRequest) req;
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+
+    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+      response.setStatus(HttpServletResponse.SC_OK);
+    } else {
+      chain.doFilter(req, resp);
     }
-    if (request.getMethod().equals("OPTIONS")) {
-      try {
-        response.getWriter().print("OK");
-        response.getWriter().flush();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }else{
-      filterChain.doFilter(request, response);
-    }
+
+  }
+
+  @Override
+  public void destroy() {
   }
 }
