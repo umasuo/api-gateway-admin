9de8092c5dfd3f2666985e6f32e2c0f22fdbe48e
davis999 <davis.dai@go6d.com>
2017-08-29 12:58:38 +0800

    add cors filter

---
+++ src/main/java/com/umasuo/gateway/admin/filters/AuthenticationPreFilter.java
@@ -0,0 +1,76 @@
+package com.umasuo.gateway.admin.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.netflix.zuul.context.RequestContext.getCurrentContext;
+
+/**
+ * CORS filter.
+ */
+//@Component
+public class AuthenticationPreFilter extends ZuulFilter {
+
+  /**
+   * Logger.
+   */
+  private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationPreFilter.class);
+
+  /**
+   * Filter type.
+   *
+   * @return
+   */
+  @Override
+  public String filterType() {
+    // use "pre", so we can check the auth before router to back end services.
+    return "pre";
+  }
+
+  /**
+   * Filter order.
+   *
+   * @return int
+   */
+  @Override
+  public int filterOrder() {
+    return 6;
+  }
+
+  /**
+   * Check if we need to run this filter for this request.
+   *
+   * @return boolean
+   */
+  @Override
+  public boolean shouldFilter() {
+    RequestContext ctx = getCurrentContext();
+    String host = ctx.getRouteHost().getHost();
+    HttpServletRequest request = ctx.getRequest();
+    String method = request.getMethod();
+    String path = request.getRequestURI();
+    LOGGER.debug("Check for host: {}, path: {}, method: {}.", host, path, method);
+    boolean shouldFilter = true;
+    if ("OPTIONS".equals(method)) {
+      LOGGER.debug("Ignore host: {}, Path: {}, action: {}.", host, path, method);
+      shouldFilter = false;
+    }
+    return shouldFilter;
+  }
+
+
+  /**
+   * Run function.
+   *
+   * @return always return null
+   */
+  @Override
+  public Object run() {
+    return null;
+  }
+}
--- src/main/java/com/umasuo/gateway/admin/filters/CorsFilter.java
+++ src/main/java/com/umasuo/gateway/admin/filters/CorsFilter.java
@@ -1,76 +1,58 @@
 package com.umasuo.gateway.admin.filters;
 
-import com.netflix.zuul.ZuulFilter;
-import com.netflix.zuul.context.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
-import static com.netflix.zuul.context.RequestContext.getCurrentContext;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
- * CORS filter.
+ * Created by Davis on 17/8/25.
  */
 @Component
-public class CorsFilter extends ZuulFilter {
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
 
   /**
    * Logger.
    */
-  private static final Logger LOGGER = LoggerFactory.getLogger(CorsFilter.class);
+  private static final Logger LOG = LoggerFactory.getLogger(CorsFilter.class);
 
-  /**
-   * Filter type.
-   *
-   * @return
-   */
+
   @Override
-  public String filterType() {
-    // use "pre", so we can check the auth before router to back end services.
-    return "pre";
+  public void init(FilterConfig fc) throws ServletException {
   }
 
-  /**
-   * Filter order.
-   *
-   * @return int
-   */
   @Override
-  public int filterOrder() {
-    return 6;
-  }
+  public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletResponse response = (HttpServletResponse) resp;
+    HttpServletRequest request = (HttpServletRequest) req;
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+    response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader("Access-Control-Allow-Headers",
+        "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
 
-  /**
-   * Check if we need to run this filter for this request.
-   *
-   * @return boolean
-   */
-  @Override
-  public boolean shouldFilter() {
-    RequestContext ctx = getCurrentContext();
-    String host = ctx.getRouteHost().getHost();
-    HttpServletRequest request = ctx.getRequest();
-    String method = request.getMethod();
-    String path = request.getRequestURI();
-    LOGGER.debug("Check for host: {}, path: {}, method: {}.", host, path, method);
-    boolean shouldFilter = true;
-    if ("OPTIONS".equals(method)) {
-      LOGGER.debug("Ignore host: {}, Path: {}, action: {}.", host, path, method);
-      shouldFilter = false;
+    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+      response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+      response.setStatus(HttpServletResponse.SC_OK);
+    } else {
+      chain.doFilter(req, resp);
     }
-    return shouldFilter;
   }
 
-
-  /**
-   * Run function.
-   *
-   * @return always return null
-   */
   @Override
-  public Object run() {
-    return null;
+  public void destroy() {
   }
 }
