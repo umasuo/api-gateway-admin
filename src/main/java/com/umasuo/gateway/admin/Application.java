8a0b4d0a582745391f0f4f080317e119f3bf166b
davis999 <davis.dai@go6d.com>
2017-08-30 10:50:15 +0800

    refactor

--- src/main/java/com/umasuo/gateway/admin/Application.java
+++ src/main/java/com/umasuo/gateway/admin/Application.java
@@ -3,19 +3,10 @@ package com.umasuo.gateway.admin;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableZuulProxy
-@RestController
 @SpringCloudApplication
 public class Application {
-
-  @GetMapping("/")
-  public String sayHello() {
-    return "Hello world";
-  }
-
 
   public static void main(String[] args) {
     new SpringApplicationBuilder(Application.class).web(true).run(args);
